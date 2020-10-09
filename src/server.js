9799f53204d396d390a0bdb8099cc6179c09590c
Ramiz Khan <ramizkhan1199@gmail.com>
2020-10-09 23:54:19 +0530

    Loaded model

--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
-const app = require("./App")
+const tf = require("@tensorflow/tfjs-node");
+const app = require("./App");
 const port = process.env.PORT || 3768;
 app.listen(port, () => {
-    console.log('Server is up on port ' + port)
-})
+    console.log("Server is up on port " + port);
+});
+
+const model = await tf.loadLayersModel("file://DL Models/BP_Model/model.json");
+model.predict();
