cdcd88522a6224538f97cab37deb3f50cb368e4e
Ramiz Khan <ramizkhan1199@gmail.com>
2020-10-10 08:15:49 +0530

    Fixed tensorflowjs issue

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 const tf = require("@tensorflow/tfjs-node");
 const app = require("./App");
 const port = process.env.PORT || 3768;
-app.listen(port, () => {
+app.listen(port, async () => {
     console.log("Server is up on port " + port);
 });
 
-const model = await tf.loadLayersModel("file://DL Models/BP_Model/model.json");
-model.predict();
+(async () => {
+    const model = await tf.loadLayersModel(
+        "file://DL Models/BP_Model/model.json"
+    );
+    // model.predict().print();
+})();
